2f8414144ecfc54018f8e21aaa2963a9f4e0a4c1
Mila Hose <mila.hose@gmail.com>
2018-09-17 06:53:06 -0700

    Add exercise 2

---
+++ 101/03_practice_problems/easy_1/02_exercise.rb
@@ -0,0 +1,20 @@
+# Describe the difference between ! and ? in Ruby.
+# And explain what would happen in the following scenarios:
+
+# what is != and where should you use it?
+# => to check inequality
+
+# put ! before something, like !user_name
+# => determine if user_name has a truthy value
+
+# put ! after something, like words.uniq!
+# => mutate the caller (depending on method)
+
+# put ? before something
+# => (depends on method)
+
+# put ? after something
+# => ternary operator (if/else)
+
+# put !! before something, like !!user_name
+# => check if it is NOT falsy (aka, truthy)
