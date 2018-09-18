8ff20504fbbf67781472cdf7287b4be9789f93a6
Mila Hose <mila.hose@gmail.com>
2018-09-18 13:29:52 -0700

    Add exercise 4

---
+++ 101/03_practice_problems/easy_1/04_exercise.rb
@@ -0,0 +1,9 @@
+# The Ruby Array class has several methods for removing items from the array.
+# Two of them have very similar names. Let's see how they differ:
+
+numbers = [1, 2, 3, 4, 5]
+
+# What do the following method calls do
+# (assume we reset numbers to the original array between method calls)?
+numbers.delete_at(1) # => [1, 3, 4, 5]
+numbers.delete(1) # => [2, 3, 4, 5]
