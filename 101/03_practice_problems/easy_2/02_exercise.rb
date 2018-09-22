6f405949ae691363ff9acc3f34b037cc7e2f6034
Mila Hose <mila.hose@gmail.com>
2018-09-21 21:17:25 -0700

    Add easy 2 practice set

---
+++ 101/03_practice_problems/easy_2/02_exercise.rb
@@ -0,0 +1,10 @@
+# Starting with this string:
+munsters_description = "The Munsters are creepy in a good way."
+
+# Convert the string in the following ways
+# (code will be executed on original munsters_description above):
+
+puts munsters_description.downcase.capitalize
+puts munsters_description.swapcase
+puts munsters_description.upcase
+puts munsters_description.downcase
---
+++ 101/03_practice_problems/easy_2/08_exercise.rb
@@ -0,0 +1,9 @@
+# Shorten this sentence:
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+# ...remove everything starting from "house".
+# Review the String#slice! documentation, and use that method to make the
+# return value "Few things in life are as important as ". But leave the
+# advice variable as "house training your pet dinosaur.".
+
+puts advice.slice!(0, advice.index('house'))
