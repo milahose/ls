c93c98f5c1190d389504f373b05232c4ca8adcd0
Mila Hose <mila.hose@gmail.com>
2018-09-29 12:29:08 -0700

    Add practice problem 6

---
+++ 101/05_advanced_collections/06_practice_problem.rb
@@ -0,0 +1,15 @@
+# Given this previously seen family hash, print out the name,
+# age and gender of each family member ...like this:
+# (Name) is a (age)-year-old (male or female).
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+munsters.each do |person, stats|
+  puts "#{person} is a #{stats['age']} #{stats['gender']}."
+end
