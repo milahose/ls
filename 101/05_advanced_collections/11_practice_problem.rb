84682edfe4b98ac24970f6c9b4b38253660bf691
Mila Hose <mila.hose@gmail.com>
2018-10-01 07:29:27 -0700

    Add practice problem 11

---
+++ 101/05_advanced_collections/11_practice_problem.rb
@@ -0,0 +1,11 @@
+# Given the following data structure use a combination of methods,
+# including either the select or reject method, to return a new array
+# identical in structure to the original but containing only the
+# integers that are multiples of 3.
+arr = [[2], [3, 5, 7], [9], [11, 13, 15]]
+
+test = arr.map do |inner_array|
+  inner_array.select { |n| n % 3 == 0 }
+end
+
+print test
