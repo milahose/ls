f8344c32e593fb1769a3f6235f684cff56e8b53b
Mila Hose <mila.hose@gmail.com>
2018-10-08 08:10:03 -0700

    Add squaring an argument

---
+++ 101/small_problems/03_easy/05_squaring_an_argument.rb
@@ -0,0 +1,13 @@
+# Using the multiply method from the "Multiplying Two Numbers" problem,
+# write a method that computes the square of its argument (the square
+# is the result of multiplying a number by itself).
+def multiply(n1, n2)
+  n1 * n2
+end
+
+def square(n)
+  multiply(n, n)
+end
+
+puts square(5) == 25
+puts square(-8) == 64
