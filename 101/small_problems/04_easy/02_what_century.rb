d90c28337eb7f49dc4603e408140c602961f3e3a
Mila Hose <mila.hose@gmail.com>
2018-10-15 07:21:49 -0700

    Add what century challenge

---
+++ 101/small_problems/04_easy/02_what_century.rb
@@ -0,0 +1,33 @@
+# Write a method that takes a year as input and returns the century. The return
+# value should be a string that begins with the century number, and ends with
+# st, nd, rd, or th as appropriate for that number.
+# New centuries begin in years that end with 01.
+# So, the years 1901-2000 comprise the 20th century.
+
+def century(year)
+  century = year / 100 + 1
+  century -= 1 if year % 100 == 0
+  century.to_s + century_suffix(century)
+end
+
+def century_suffix(century)
+  return 'th' if [11, 12, 13].include?(century % 100)
+  last_digit = century % 10
+
+  case last_digit
+  when 1 then 'st'
+  when 2 then 'nd'
+  when 3 then 'rd'
+  else 'th'
+  end
+end
+
+puts century(2000) == '20th'
+puts century(2001) == '21st'
+puts century(1965) == '20th'
+puts century(256) == '3rd'
+puts century(5) == '1st'
+puts century(10103) == '102nd'
+puts century(1052) == '11th'
+puts century(1127) == '12th'
+puts century(11201) == '113th'
