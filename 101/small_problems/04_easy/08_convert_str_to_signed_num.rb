fc94fbcd0d0a169eb8bc336960fa63f82a787ec2
Mila Hose <mila.hose@gmail.com>
2018-10-22 07:13:27 -0700

    Add convert str to signed num exercise

---
+++ 101/small_problems/04_easy/08_convert_str_to_signed_num.rb
@@ -0,0 +1,33 @@
+# Write a method that takes a String of digits, and returns the appropriate
+# number as an integer. The String may have a leading + or - sign; if the first
+# character is a +, your method should return a positive number; if it is a -,
+# your method should return a negative number. If no sign is given, you should
+# return a positive number.
+DIGITS = {
+  '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
+  '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9
+}
+
+def string_to_integer(str)
+  digits = str.chars.map { |char| DIGITS[char] }
+
+  value = 0
+  digits.each { |digit| value = 10 * value + digit }
+  value
+end
+
+def string_to_signed_integer(str)
+  signed_int = 0
+  if str[0] == '-'
+    signed_int = (0 - string_to_integer(str[1..str.length]))
+  elsif str[0] == '+'
+    signed_int = (string_to_integer(str[1..str.length]))
+  else
+    signed_int = string_to_integer(str)
+  end
+  signed_int
+end
+
+puts string_to_signed_integer('4321') == 4321
+puts string_to_signed_integer('-570') == -570
+puts string_to_signed_integer('+100') == 100
